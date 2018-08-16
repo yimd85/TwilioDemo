287ade055c7b4d469186d41b4f2b9571ef4be397
David Yim <yimd85@gmail.com>
2018-08-15 22:28:01 -0400

    added sms response

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// const express = require('express');
-//
-// const app = express();
-// const port = process.env.PORT || 3000;
-require('dotenv').config();
+// const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config();
 
 const accountSid = process.env.ACCOUNTSID;
 const authToken = process.env.AUTHTOKEN;
@@ -11,15 +12,27 @@ const client = require('twilio')(accountSid, authToken);
 
 
 // app.get('/', (req, res) => {
-//     res.send(process.env.MY_PHONE_NUMBER);
-// });
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}.`);
+//     res.send('I love you so much Seyeon');
 // });
 
+app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+    twiml.message('This is God responding back');
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+});
 
 client.messages.create({
   to: process.env.MY_PHONE_NUMBER,
   from: '+16468462045 ',
-  body: 'this is amazing'
-}).then ((message) => console.log(message.sid));
+  body: 'This is GOD. you are amazing. keep on doing your thing. Now, go in peace serve the lord'
+}).then((message) => console.log(message.sid));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
+
+// http.createServer(app).listen(3000, () => {
+//   console.log(`express app listening on 3000.`);
+// });
